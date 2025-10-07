75d15cb00f0559df6a57a5587443ac6805029f0e
Lollicry <2503kirill@gmail.com>
2025-10-07 15:00:13 +0400

    Убран 1 баг

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,17 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-//        //Task1
-//        Task1.run();
-//
-//        //Task2
-//        Task2.run();
-//
-//        //Task3
-//        Task3.run();
-//
-//        //Task4
-//        Task4.run();
+        //Task1
+        Task1.run();
+
+        //Task2
+        Task2.run();
+
+        //Task3
+        Task3.run();
+
+        //Task4
+        Task4.run();
 
         //Task5
         Task5.run();
